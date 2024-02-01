3fd11a434c31e0ba35c1e682722f700595ed4acc
Spy <109648665+CrisAmorim27@users.noreply.github.com>
2024-02-01 03:04:29 -0300

    Update infodono.js

--- dono/menus/infodono.js
+++ dono/menus/infodono.js
@@ -4,15 +4,15 @@ const infodono = (prefix, numerodn, NomeDoBot, sender) => {
 //${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
 
 return`
-╭───────────────┐
+╭─s──────────────┐
 ├ INFORMAÇÕES DO DONO
-├───────────────
+├─────p──────────
 │ Solicitou o CMD: @${sender.split("@")[0]}
-├───────────────┐
-│ Dono: wa.me/${numerodn} 
-╞───────────────┘
-╰──────────┐
-╭──────────┴─┐
+├──────────y─────┐
+│ Dono: wa.me/554884702848
+╞─s──────────────┘
+╰─────p─────┐
+╭──────────y┴─┐
 │ INFORMAÇÕES DO BOT
 ├────────────
 │Prefixo: ⩺ ${prefix}
